17204348817eadb1172b31431c7efd5872aab02f
Ming Duan <11203duan@gmail.com>
2019-02-17 02:36:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,7 +10,7 @@ def reformat_languages(languages)
       arr << category
         attributes[:style] = arr
       else
-        new_hash[:name][:style] << category
+        new_hash[name][:style] << category
         end
       end
     end
